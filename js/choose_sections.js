8a32b79df94ea6e570d5e108cb386e4cbe243a9e
akkaraquake <almaz.aiupov@mail.ru>
2024-02-20 13:35:43 +0300

    one more location determination fix

--- js/choose_sections.js
+++ js/choose_sections.js
@@ -1,5 +1,5 @@
 $('#select_sections_btn').on('click', function() {
-    $(location).prop('href', 'https://samotele.com/sbornik.php');
+    $(location).prop('href', 'http://samotele.com/sbornik.php');
 
     localStorage.setItem('choose_sections_request', 'true');
 })
